e660f6123baf1b77008c4bf42d9ce5332c51035e
crypticaarya <sarthakghoderao@gmail.com>
2025-06-20 14:35:05 +0530

    Add static Appointment Booking UI with form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import Navbar from "./comopents/navbar";
-import Register from "./pages/register";
+import React from 'react';
+import './App.css';
+
+import Navbar from './comopents/navbar';
+import Register from './pages/register';
+import AppointmentPage from './pages/AppointmentPage';
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Register/>
+      <Navbar />
+      <Register />
+      <AppointmentPage /> {/* ✅ Add this line to show your booking UI */}
     </div>
   );
 }
